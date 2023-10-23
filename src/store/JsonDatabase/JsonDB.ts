d8ce5b61e464a5e5efc57385297cddb1a645f558
George007361 <gera.illarionov@mail.ru>
2023-10-23 22:56:28 +0300

    Storage interface, simple Json implimentation

---
+++ src/store/JsonDatabase/JsonDB.ts
@@ -0,0 +1,45 @@
+import { JsonDB, Config } from 'node-json-db';
+import Store from '../store';
+
+export class JsonStorage implements Store {
+    db: JsonDB;
+    constructor(path: string) {
+        this.db = new JsonDB(new Config(path, true, true, '/'));
+    }
+
+    public async getInternalChatId(peer_id: number, group_id: number): Promise<number | undefined> {
+        const path = '/chats/' + String(peer_id) + String(group_id);
+        console.log('DB: get ', path);
+
+        try {
+            const data = await this.db.getData(path);
+            return Promise.resolve(Number(data));
+
+        } catch (e) {
+            console.log('DB get error ', e);
+            return Promise.resolve(undefined);
+        }
+    }
+
+    public async setInternalChatId(peer_id: number, group_id: number, chat_id: number): Promise<boolean> {
+        const path = '/chats/' + String(peer_id) + String(group_id);
+
+        const check_id = await this.getInternalChatId(peer_id, group_id);
+
+        if (check_id) {
+            console.log('DB set ', path, 'exists');
+            return Promise.resolve(false);
+        }
+
+
+        try {
+            await this.db.push(path, chat_id);
+            console.log('DB set ', path, ' success');
+            return Promise.resolve(true);
+        } catch (e) {
+            console.log('DB set error ', e);
+            return Promise.resolve(false);
+        }
+    }
+
+}
