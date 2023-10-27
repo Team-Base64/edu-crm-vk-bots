597b5b64058370f05bf7aa7b8219645a54f0adb3
George007361 <gera.illarionov@mail.ru>
2023-10-28 00:58:25 +0300

    add logger, refactor graceful stop

--- app/helpers/graceful-stop.ts
+++ app/helpers/graceful-stop.ts
@@ -1,11 +1,30 @@
+import logger from "./logger";
 
-export const gracefulStop = (handler: Function) => {
-    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-        'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM', 'exit'
-    ].forEach(signal => {
-        process.on(signal, async () => {
-            await handler();
-            process.exit();
-        });
+type ExitHandler = (...args: any) => any;
+
+const handlers: ExitHandler[] = [];
+const timeout = 5000; // 5 sec
+
+const wrapper = () => {
+    setTimeout(() => { 
+        logger.info('Exitted by timeout');
+        process.exit(0)
+     }, timeout);
+
+    handlers.forEach(async (h) => {
+        await h();
     });
+
+    logger.info('Exitted normally');
+    process.exit(0);
+}
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+    'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM', 'exit'
+].forEach(signal => {
+    process.on(signal, wrapper);
+});
+
+export const gracefulStop = (handler: ExitHandler) => {
+    handlers.push(handler);
 }
--- app/index.ts
+++ app/index.ts
@@ -2,24 +2,24 @@ import GRPCBackend from "./backend/GRPC-backend/grpc-backend";
 import BackendMock from "./backend/mock/backend";
 import VkBotsManager from "./manager/manager";
 import { PostrgesStore } from "./store/PostrgeSQL/postrgesql";
-import {postgres_config_docker} from "./store/PostrgeSQL/config";
+import {postgres_config_docker, postgres_config_localhost} from "./store/PostrgeSQL/config";
 import {grpc_config} from "./backend/GRPC-backend/config";
+import logger from "./helpers/logger";
 
 const grpc = require('@grpc/grpc-js');
 
-
 // Main function
 (async () => {
-    console.log("Hello vk slave bots");
+    logger.info('Starting app...');
 
     // Init database
-    const db = new PostrgesStore(postgres_config_docker);
+    const db = new PostrgesStore(postgres_config_localhost);
     await db.start();
 
     // Init backend
 
     // Mock backend
-    const backend = new BackendMock(postgres_config_docker);
+    const backend = new BackendMock(postgres_config_localhost);
     await backend.start();
 
     // GRPC remote backend
