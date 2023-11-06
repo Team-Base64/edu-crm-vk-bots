02291139a8851350f12163465ac49a2f60dc898b
George Illarionov <92120286+george007361@users.noreply.github.com>
2023-11-06 15:03:03 +0300

    Educrm 0.4 refactor remove old files (#3)
    * VkBot shared class implimentation, VkSlaveBot simple, graceful stopping bots
    
    * Storage interface, simple Json implimentation
    
    * slave bot: handle message, refactor store
    
    * Masterbot: /start, /accept token commands
    
    * change Store interface
    
    * create postges store, sql tables + initial data for test
    
    * implement postgres store
    
    * fix JSON store for compiling, DID NOT USE IT
    
    * move helpers to root dir
    
    * fix tsconfig
    
    * fix package.json to work with postrgres
    
    * add mocked Backend
    
    * add docker with network for tests
    
    * move connection to db to bk-bot
    
    * fix slave bot
    
    * Add tokenHandler to accept scene
    
    * add handlToken to master bot
    
    * change index.ts to run master bot
    
    * Dockerfile for app
    
    * remove old files
    
    * added 2nd Slave bot, mock function send Bot->Backend, add check attach user to chat
    
    * grpc backend started, create backend interface
    
    * reafactor bots
    
    * testing grpc
    
    * run via ts-node TODO!
    
    * hardcode createChat TODO
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * fix receive msg
    
    * fix receive msg Postgres field
    
    * fix receive msg Postgres field
    
    * add comments, change interfaces
    
    * refactor: app and db folders, configs to connect grps, postrges
    
    * refactor: app and db folders, configs to connect grps, postrges
    
    * add logger, refactor graceful stop
    
    * remove console log, add logger
    
    * change dockerfiles
    
    * fix docker files
    
    * remove files after refactor

--- src/store/store.ts
+++
@@ -1,7 +0,0 @@
-export default abstract class Store {
-    public abstract getInternalChatId(peer_id: number, group_id: number): Promise<number | undefined>;
-
-    public abstract setInternalChatId(peer_id: number, group_id: number, chat_id: number): Promise<boolean>;
-
-    public abstract getFreeSlaveBots(peer_id: number) : Promise<number[] | undefined>;
-}
