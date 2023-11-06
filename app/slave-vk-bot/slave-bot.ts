17db08d79eb0816e8ad5531a03eb992192028f9e
George007361 <gera.illarionov@mail.ru>
2023-11-06 22:45:59 +0300

    add attaches to slave bot

--- app/slave-vk-bot/slave-bot.ts
+++ app/slave-vk-bot/slave-bot.ts
@@ -5,6 +5,9 @@ import Backend from "../backend/backend";
 import { Attachment, AttachmentType, ContextDefaultState, MessageContext, PhotoAttachment } from "vk-io";
 import logger from "../helpers/logger";
 
+import mime from 'mime';
+import { changeHttpsToHttp } from "../helpers/changeHttpsToHttp";
+
 const slaveBotLogger = logger.child({ class: 'SlaveVkBot' });
 
 export default class VkSlaveBot extends VkBot {
@@ -25,7 +28,7 @@ export default class VkSlaveBot extends VkBot {
 
     private async handleMessage(context: MessageContext<ContextDefaultState>): Promise<boolean> {
         const { peerId, $groupId, text, attachments } = context;
-        slaveBotLogger.debug({peerId}, 'Get message ');
+        slaveBotLogger.debug({ peerId }, 'Get message ');
 
         if (!$groupId) {
             slaveBotLogger.warn('Group id in vk message is undefined ');
@@ -46,28 +49,85 @@ export default class VkSlaveBot extends VkBot {
         }
 
         // Работа с вложениями
-        // Проверить есть ли вложения
-        const attachmentUrls :string[] =  [];
-        if(attachments){
-            // для каждого вложения
-            attachments.forEach((a)=> {
-                // Если фото
-                if(a.type === AttachmentType.PHOTO){
-                   // Получаем url
-                    const {largeSizeUrl} : any = a.toJSON(); 
-                    if(largeSizeUrl){
-                        attachmentUrls.push(largeSizeUrl);
-                    } 
+
+        const uploaded_attaches: string[] = [];
+
+        // для каждого вложения
+        for (let attach of attachments) {
+            const attachJson = attach.toJSON() as any;
+
+            // Если документ
+            if (attach.type === AttachmentType.DOCUMENT) {
+                // url
+                const { url, extension }: { url: string, extension: string } = attachJson;
+
+                // mimetype
+                const mimetype = mime.getType(extension);
+                if (!mimetype) continue;
+
+                // Загружаем на бэк
+                const { internalFileURL, ...uploadFileError } =
+                    await this.backend.uploadFile({
+                        fileURL: changeHttpsToHttp(url),
+                        mimetype: mimetype
+                    });
+
+                if (uploadFileError.isError) {
+                    slaveBotLogger.warn(uploadFileError.error, 'Upload document error');
+                    continue;
                 }
-            });
+
+                // Сохраняем внутренний путь
+                uploaded_attaches.push(internalFileURL);
+                continue;
+            }
+
+            // Если фото
+            if (attach.type === AttachmentType.PHOTO) {
+                // Получаем url
+                const fullURL: string = attachJson.largeSizeUrl;
+                if (!fullURL) continue;
+
+                // mimetype
+                const mimetype = mime.getType(
+                    fullURL
+                        .slice(
+                            0,
+                            fullURL.indexOf('?')
+                        )
+                );
+
+                if (!mimetype) continue;
+
+                // Загружаем на бэк
+                const { internalFileURL, ...uploadFileError } =
+                    await this.backend.uploadFile({
+                        fileURL: changeHttpsToHttp(fullURL),
+                        mimetype: mimetype
+                    });
+
+                if (uploadFileError.isError) {
+                    slaveBotLogger.warn(uploadFileError.error, 'Upload photo error');
+                    continue;
+                }
+
+                // Сохраняем внутренний путь
+                uploaded_attaches.push(internalFileURL);
+                continue;
+            }
         }
-        
-        
+
+
 
         // Собрать сообщение
         // Отправить сообщение на бэкэнд
         slaveBotLogger.debug('Sending msg to backend');
-        const isOk = await this.backend.resendMessageFromClient(internal_chat_id, text || '');
+        const isOk = await this.backend.resendMessageFromClient({
+            internal_chat_id: internal_chat_id,
+            text: text || '',
+            attachmentURLs: uploaded_attaches,
+        });
+
         if (!isOk) {
             slaveBotLogger.warn('Sending msg to backend failed');
             this.sendMessageToClient(peerId, 'Сообщение не доставлено');
